b04d44c26516dfcfc5dda78d6c6d8aab631e0fd4
LokiDarkness <lokijuliver@gmail.com>
2026-01-15 00:32:51 +0700

    big update

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI,WebSocket,WebSocketDisconnect,Query
 import asyncio,json
-from game import roll_dice,calc_reward,ICONS
+from collections import Counter
+from game import roll_dice,calc_reward
 from rooms import rooms,create_room,join_room,remove_player
 
 app=FastAPI()
@@ -13,81 +14,44 @@ async def lobby(ws:WebSocket):
     try:
         while True:
             d=json.loads(await ws.receive_text())
-            if d["type"]=="CHAT":
-                for c in lobby_clients[:]:
-                    try: await c.send_json(d)
-                    except: lobby_clients.remove(c)
-    except WebSocketDisconnect:
+            for c in lobby_clients:
+                await c.send_json(d)
+    except:
         lobby_clients.remove(ws)
 
-async def broadcast_rooms():
-    for c in lobby_clients[:]:
-        try:
-            await c.send_json({
-                "type":"ROOM_LIST",
-                "rooms":[{"id":r} for r in rooms]
-            })
-        except: lobby_clients.remove(c)
-
-@app.websocket("/ws/{room_id}/{uid}")
-async def game_ws(ws:WebSocket,room_id:str,uid:str,pw:str=Query(""),name:str=Query("")):
+@app.websocket("/ws/{room}/{uid}")
+async def game_ws(ws:WebSocket,room:str,uid:str,pw:str=Query(""),name:str=Query("")):
     await ws.accept()
+    if room not in rooms:
+        create_room(room,uid,pw,name)
+    elif not join_room(room,uid,pw,name):
+        await ws.close();return
 
-    if room_id not in rooms:
-        create_room(room_id,uid,pw,name)
-        await broadcast_rooms()
-    elif not join_room(room_id,uid,pw,name):
-        await ws.close(); return
-
-    room=rooms[room_id]
-    room["clients"][uid]=ws
-
-    await ws.send_json({
-        "type":"PLAYER_UPDATE",
-        "players":room["players"],
-        "host":room["host"]
-    })
+    r=rooms[room]
+    r["clients"][uid]=ws
 
     try:
         while True:
             d=json.loads(await ws.receive_text())
+            if d["type"]=="HOST_ROLL" and uid==r["host"]:
+                for c in r["clients"].values():
+                    await c.send_json({"type":"ROLL_COUNTDOWN","from":3})
+                await asyncio.sleep(3)
 
-            if d["type"]=="PLACE_BET":
-                p=room["players"][uid]
-                icon,amt=d["icon"],int(d["amount"])
-                if icon in ICONS and p["money"]>=amt>0:
-                    p["money"]-=amt
-                    p["bets"][icon]=p["bets"].get(icon,0)+amt
-                    for c in room["clients"].values():
-                        await c.send_json({
-                            "type":"BET_UPDATE",
-                            "uid":uid,
-                            "icon":icon,
-                            "amount":p["bets"][icon],
-                            "money":p["money"]
-                        })
-
-            if d["type"]=="HOST_ROLL" and uid==room["host"]:
                 dice=roll_dice()
-                for c in room["clients"].values():
-                    await c.send_json({"type":"ROLL_START"})
-                await asyncio.sleep(2)
-                for p in room["players"].values():
+                wins=dict(Counter(dice))
+
+                for p in r["players"].values():
                     p["money"]+=calc_reward(p["bets"],dice)
                     p["bets"]={}
-                for c in room["clients"].values():
+
+                for c in r["clients"].values():
                     await c.send_json({
                         "type":"ROLL_RESULT",
                         "dice":dice,
-                        "players":room["players"],
-                        "host":room["host"]
+                        "wins":wins,
+                        "players":r["players"],
+                        "host":r["host"]
                     })
     except WebSocketDisconnect:
-        new_host=remove_player(room_id,uid)
-        if room_id in rooms and new_host:
-            for c in rooms[room_id]["clients"].values():
-                await c.send_json({
-                    "type":"SYSTEM",
-                    "msg":f"👑 {rooms[room_id]['players'][new_host]['name']} đã trở thành HOST"
-                })
-        await broadcast_rooms()
+        remove_player(room,uid)
